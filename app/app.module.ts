e6db644759ccfdfa522924793333466d663c3282
Joseph M J <joseph.mj@cordiant.com>
2016-08-11 17:13:02 +0530

    http.get for service

--- app/app.module.ts
+++ app/app.module.ts
@@ -3,12 +3,13 @@
  */
 import {NgModule}      from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
+import { HttpModule }     from '@angular/http';
 
 import {AppComponent}  from './app.component';
 import {HeroService} from "./hero.service";
 
 @NgModule({
-    imports: [BrowserModule],
+    imports: [BrowserModule,HttpModule],
     declarations: [AppComponent],
     bootstrap: [AppComponent],
     providers: [HeroService]
--- app/hero.service.ts
+++ app/hero.service.ts
@@ -1,9 +1,33 @@
 import {Injectable} from '@angular/core';
 import {HEROES} from './mock-heroes';
+import {Http}     from '@angular/http';
+import 'rxjs/add/operator/toPromise';
+
+import {Hero} from "./hero";
 @Injectable()
 export class HeroService {
+    /* getHeroes() {
+     // return HEROES;
+     return Promise.resolve(HEROES);
+     }*/
+
+    private heroesUrl = 'http://localhost/json/heros.php';  // URL to web api
+
+    constructor(private http:Http) {
+    }
+
+    /**  Using Http.get */
     getHeroes() {
-        // return HEROES;
-        return Promise.resolve(HEROES);
+        return this.http.get(this.heroesUrl)
+            .toPromise()
+            .then(response => response.json().data as Hero[])
+            .catch(this.handleError);
+    }
+
+    
+
+    private handleError(error:any) {
+        console.error('An error occurred', error);
+        return Promise.reject(error.message || error);
     }
 }
